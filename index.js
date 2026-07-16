4a7dde68e60aeb74b332b21a241b748baaa0fe8c
Marvellous <areomarvell06@gmail.com>
2026-07-16 09:06:02 +0100

    feat: add new songs to musicians array

--- index.js
+++ index.js
@@ -35,7 +35,43 @@ const musicians = [
       image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNWlkmpeyLRzuEcDjD9fDXsgpCheOQLzMKeWtmphsVeA&s=10",
       SongName: "Where are you ",
       SongUrl: "https://res.cloudinary.com/r5nd34og/video/upload/v1784185560/AUD-20260522-WA0024_ovxanj.mp3"
+    },
+    {
+        id: 6,
+        ArtistName: "David kushner",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5vCxcjod59t6d4-FJQgOkvhTLZ6oaIPhkoYPl5ECU5Q&s=10",
+        SongName: "Daylight",
+        SongUrl: "https://res.cloudinary.com/r5nd34og/video/upload/v1784188091/AUD-20260522-WA0013_qsangt.mp3"
+    },
+    {
+        id: 7,
+        ArtistName: "Halsey",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsnR5tHECqXI6UZFLofvnlJFvs31Wrz6wgsOPyQDoqeg&s=10",
+        SongName: "Without me",
+        SongUrl: "https://res.cloudinary.com/r5nd34og/video/upload/v1784188320/AUD-20260522-WA0016_rmkmpc.mp3"
+    },
+    {
+        id: 8,
+        ArtistName: "sia",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6-1ZFyohlwIG2cPldOQENrjI6966UrI_FERRr5ZqvFw&s=10",
+        SongName: "chandelier",
+        SongUrl: "https://res.cloudinary.com/r5nd34og/video/upload/v1784188538/AUD-20260522-WA0022_dlw8mz.mp3"
+    },
+    {
+        id: 9,
+        ArtistName: "Demi Lovato",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuCH8zAz6zCkZpsDnDzEztGRINHwcP-ZhnATtBvKaMtA&s=10",
+        SongName: "Heart Attack",
+        SongUrl: "https://res.cloudinary.com/r5nd34og/video/upload/v1784188774/AUD-20260522-WA0026_tjllhh.mp3"
+    },
+    {
+        id: 10,
+        ArtistName: "Conor Maynard",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqQWA5gZg_nReoaphUs-EKTgtIpY6mHeeGdHd0HFm2nQ&s=10",
+        SongName: "Someone you loved",
+        SongUrl: "https://res.cloudinary.com/r5nd34og/video/upload/v1784188962/AUD-20260522-WA0017_scufna.mp3"
     }
+
 ]
 
 app.get("/", (req, res) => {
